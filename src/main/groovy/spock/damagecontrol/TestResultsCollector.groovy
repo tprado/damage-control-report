239271af8406518cf3c59132671832f603075732
Thiago Prado <tprado@thoughtworks.com>
2012-07-19 15:36:12 -0500

    Thiago: refactored TestResultsCollector.collect into several methods.

--- src/main/groovy/spock/damagecontrol/TestResultsCollector.groovy
+++ src/main/groovy/spock/damagecontrol/TestResultsCollector.groovy
@@ -7,12 +7,20 @@ class TestResultsCollector {
     def collect(reader) {
         Node testSuite = new XmlParser().parse(reader)
 
+        collectTestSuite(testSuite)
+        collectTestCases(testSuite)
+        collectIgnoredTestCases(testSuite)
+    }
+
+    private collectTestSuite(testSuite) {
         Spec spec = results.spec(testSuite.'@name')
 
         spec.duration = testSuite.'@time'
         spec.output.standard = testSuite.'system-out' ? testSuite.'system-out'[0].text() : ''
         spec.output.error = testSuite.'system-err' ? testSuite.'system-err'[0].text() : ''
-        
+    }
+
+    private collectTestCases(testSuite) {
         testSuite.'testcase'.each { testCase ->
             def feature
 
@@ -28,7 +36,9 @@ class TestResultsCollector {
 
             feature.duration = testCase.'@time'
         }
+    }
 
+    private collectIgnoredTestCases(testSuite) {
         testSuite.'ignored-testcase'.each { testCase ->
             results.spec(testCase.'@classname').skipped(testCase.'@name')
         }
