485c62e03d861e628e66b47184e153e3ce808491
Thiago Prado <tprado@thoughtworks.com>
2013-07-17 19:46:17 -0300

    Dummy change to trigger travis-ci.

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [[ ! $M2_HOME ]]; then
+    echo 'M2_HOME env var not defined.'
+
     MAVEN_COMMAND_PATH="$(which mvn)"
     if [[ -h $MAVEN_COMMAND_PATH ]]; then
         MAVEN_COMMAND_PATH="$(readlink $MAVEN_COMMAND_PATH)"
