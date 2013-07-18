a527d67ca725a6546f6c9b4da6413b3b4ae62c2f
Thiago Prado <tprado@thoughtworks.com>
2013-07-17 22:51:04 -0300

    Building just 'html-generator' module.

--- travis-build.sh
+++ travis-build.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-if [[ ! $M2_HOME ]]; then
-    echo 'M2_HOME env var not defined.'
-
-    MAVEN_COMMAND_PATH="$(which mvn)"
-    if [[ -h $MAVEN_COMMAND_PATH ]]; then
-        MAVEN_COMMAND_PATH="$(readlink $MAVEN_COMMAND_PATH)"
-    fi
-    export M2_HOME="${MAVEN_COMMAND_PATH%/bin/mvn}"
-fi
-
-echo
-echo "M2_HOME=$M2_HOME"
-echo
-
-gradle assemble
+gradle --info :html-generator:assemble
