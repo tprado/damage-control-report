82ba4c82c940b034dce81efbac86a6443a6e82d2
Thiago Prado <tprado@thoughtworks.com>
2013-07-17 23:22:35 -0300

    Executing gradle within module's directory.

--- travis-build.sh
+++ travis-build.sh
@@ -3,6 +3,12 @@
 echo
 echo 'Building html-generator module...'
 echo '-------------------------------------------------------------------'
-gradle --info :html-generator:clean :html-generator:install
+cd html-generator
+gradle install &> build.output
+HTML_GENERATOR_RESULT=$?
+cat build.output
 echo '-------------------------------------------------------------------'
 
+cd ..
+
+exit $HTML_GENERATOR_RESULT
