acff72f622a980f2bec9b5883ae6462281c77194
Thiago Prado <tprado@thoughtworks.com>
2014-07-08 17:33:38 -0300

    Does not filter build output.

--- build.sh
+++ build.sh
@@ -10,22 +10,8 @@ echo
 run_gradle() {
     local task=$1
 
-    gradle --info ${task} 1> build.output 2> build.error
-    local result=$?
+    gradle --info ${task}
 
-    echo 'build output:'
-    echo
-    cat build.output | grep -v -i 'download.*' | grep -v -i '^\s*download.*' | grep -v '\s*K\{0,1\}B\s*$' | sed 's/^\s*$//g' | cat -s
-    echo
-
-    if [[ ${result} != 0 ]]; then
-        echo 'build error:'
-        echo
-        cat build.error
-        echo
-
-        exit ${result}
-    fi
 }
 
 echo 'building artifacts...'
